e12a87b0cdfbe35b35580c37d76c4b6e5e56eb1e
Gifari Kautsar <gifari13@gmail.com>
2020-11-04 01:36:31 -0800

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // Using + operatior to create variables to numbers to avoid stirng concatenation.
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
